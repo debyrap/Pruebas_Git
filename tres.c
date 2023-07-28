4b3aaa97fafe867f7f0b9b06842d982328d1c320
DeboraRaposo <deboralilianaraposo@gmail.com>
2023-07-28 12:06:27 -0300

    Segundas pruebas

--- tres.c
+++ tres.c
@@ -1,1 +1,23 @@
 include <stdio.h>
+      
+
+int i; 
+int n; 
+
+int main (void){
+
+    printf ("Ingresar el tamaño que tendrá el vector: \n");
+    scanf ("%i", &n);
+    int v[n];
+    printf ("Ingresar los valores que tedrá el vector: \n");
+    for (i = 0; i < n; i++) {
+    scanf ("%i", &v[i]);
+    }
+
+    printf ("El vector elegido fue de %i\n", n);
+    printf ("Los valores ingresados fueron: \n");
+    for (i = 0; i < n; i++) {
+    printf ("%i", v[i]);
+    }
+
+}
